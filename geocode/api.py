e2f892e39cc3c98ce273fe7c35d25dcccbb1361b
20tabsrl <20tab@powertab.local>
2014-05-20 14:42:38 +0200

    address in data

--- geocode/api.py
+++ geocode/api.py
@@ -6,19 +6,19 @@ class GeocodeClient(object):
     url = "https://maps.googleapis.com/maps/api/geocode"
     api_type = "json"
 
-    def __init__(self, address, key, data={}, url=None, api_type=None):
-        self.address = address
-        self._prepare_address()
+    def __init__(self, key, data={}, url=None, api_type=None):
         self.key = key
         self.data = data
+        self._prepare_address()
         if url:
             self.url = url
         if api_type:
             self.api_type = api_type
 
     def _prepare_address(self):
-        splitted = [w for w in self.address.split(' ') if w]
-        self.address = u"+".join(splitted)
+        if u'address' in self.data:
+            splitted = [w for w in self.data['address'].split(' ') if w]
+            data['address'] = u"+".join(splitted)
 
     def _data_join(self):
         qs_list = [u'{}={}'.format(k, v) for k, v in self.data.items()]
@@ -26,7 +26,6 @@ class GeocodeClient(object):
 
     def uri(self):
         self.data[u'key'] = self.key
-        self.data[u'address'] = self.address
 
         return u'{}/{}?{}'.format(
             self.url,
