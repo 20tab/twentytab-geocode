1191e353091b4ac42e3fdeccf01b9641d6c49aa9
20tabsrl <20tab@powertab.local>
2014-05-20 12:49:25 +0200

    installer

--- geocode/api.py
+++ geocode/api.py
@@ -1,13 +1,20 @@
-from django.conf import settings
 import requests
 
 
 class GeocodeClient(object):
 
-    def __init__(self, address, data={}):
+    url = "https://maps.googleapis.com/maps/api/geocode"
+    api_type = "json"
+
+    def __init__(self, address, key, data={}, url=None, api_type=None):
         self.address = address
         self._prepare_address()
+        self.key = key
         self.data = data
+        if url:
+            self.url = url
+        if api_type:
+            self.api_type = api_type
 
     def _prepare_address(self):
         splitted = [w for w in self.address.split(' ') if w]
@@ -18,13 +25,12 @@ class GeocodeClient(object):
         return u'&'.join(qs_list)
 
     def get(self):
-        url_api = settings.GMAPS_API_URL
-        self.data[u'key'] = settings.GMAPS_API_KEY
+        self.data[u'key'] = self.key
         self.data[u'address'] = self.address
 
         path = u'{}/{}?{}'.format(
-            url_api,
-            settings.GMAPS_API_TYPE,
+            self.url,
+            self.api_type,
             self._data_join()
         )
 
--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ import geocode
 setup(
     name='twentytab-geocode',
     version=geocode.__version__,
-    description='A django app that initializes admin changelist view with select filters usin jquery-plugin select2',
+    description='It\'s a simple python client that uses google geocode api',
     author='20tab S.r.l.',
     author_email='info@20tab.com',
     url='https://github.com/20tab/twentytab-geocode',
     license='MIT License',
     install_requires=[
         'requests>=2',
-        'django-appconf'
     ],
     packages=find_packages(),
     include_package_data=True,
